5e699b68c54d704468dee6d502db2159a1a472ab
ubuntu <mhmd01qk@gmail.com>
2021-01-09 22:59:39 +0330

    write code for 3 method(txt, json, API)

---
+++ js/app.js
@@ -0,0 +1,46 @@
+document.querySelector('#button1').addEventListener('click', loadData)
+document.querySelector('#button2').addEventListener('click', loadJSON)
+document.querySelector('#button3').addEventListener('click', loadAPI)
+
+function loadData(){
+     fetch('../data.txt')
+     .then((response) =>  response.text())
+     .then((data) => {
+        document.querySelector('#result').innerHTML = data
+     })
+}
+
+function loadJSON(){
+    fetch('../employees.json')
+    .then((response) => response.json())
+    .then((json) => {
+        let html = ''
+
+        json.forEach(person => {
+            html += `
+                <li>
+                <b>Name </b> = ${ person.name} __ 
+                <b>Job </b> = ${ person.job}
+                </li>           
+            `
+        document.querySelector('#result').innerHTML = html
+        });
+    })
+}
+
+function loadAPI(){
+    fetch('https://picsum.photos/list')
+    .then((response) => response.json())
+    
+    .then((images) => {
+        let html = ''
+        images.forEach(image => {
+            html +=`
+            <li>
+                <a href="${image.post_url}">view image</a>
+            </li>             
+            `            
+        });
+        document.querySelector('#result').innerHTML = html
+    })
+}
